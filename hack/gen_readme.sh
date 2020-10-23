f1bcd5ef9bf9d053f0086b1a4b938577a360d395
Duyet Le <lvduyet@fossil.com>
2020-10-23 19:10:14 +0700

    Update to Spark 3.0.1

--- hack/gen_readme.sh
+++ hack/gen_readme.sh
@@ -5,7 +5,7 @@ GIT_REPO="duyetdev/spark-history-server-docker"
 DOCKERHUB_IMAGE_NAME="duyetdev/spark-history-server"
 GITHUB_DOCKER_IMAGE_NAME="spark-history-server"
 PLATFORMS="aws gcp azure"
-SPARK_VERSION="v2.4.0 latest"
+SPARK_VERSION="v3.0.1 v2.4.0 latest"
 OUTFILE="README.md"
 
 cat <<'EOF' >$OUTFILE
@@ -13,12 +13,12 @@ cat <<'EOF' >$OUTFILE
 
 Docker image for Spark history Server on Kubernetes
 
-# Supported tags
+## Supported tags
 
 EOF
 
 for PLATFORM in $PLATFORMS; do
-    printf ' - ' >> $OUTFILE
+    printf '- ' >> $OUTFILE
 
     for VERSION in $SPARK_VERSION; do
         printf "\`$PLATFORM-$VERSION\` " >> $OUTFILE
@@ -30,21 +30,21 @@ done
 
 
 cat <<EOF >>$OUTFILE
-# Pull image
+## Pull image
 
-## Github Docker
+### Github Docker
 
 Package: https://github.com/duyetdev/spark-history-server-docker/packages/238883
 
-\`\`\`
+\`\`\`bash
 docker pull docker.pkg.github.com/${GIT_REPO}/$GITHUB_DOCKER_IMAGE_NAME:<tag>
 \`\`\`
 
-## Docker Hub
+### Docker Hub
 
 Docker Hub: https://hub.docker.com/r/duyetdev/spark-history-server
 
-\`\`\`
+\`\`\`bash
 docker pull $DOCKERHUB_IMAGE_NAME:<tag>
 \`\`\`
 EOF
